cbe66cd0be6bbe739e49d5a0e7f9eed5fc5debd7
spzSource <sasha.popitich@gmail.com>
2023-12-30 23:50:35 +0200

    added clip & tours

--- src/komoot.rs
+++ src/komoot.rs
@@ -1,9 +1,20 @@
+use chrono::{FixedOffset, Local};
+use clap::{self, Args};
 use serde::Deserialize;
+
+#[derive(Debug, Args)]
+pub struct KomootOpts {
+    #[clap(short = 'u', long = "user-name")]
+    pub user_name: String,
+
+    #[clap(short = 'p', long = "password")]
+    pub password: String,
+}
 
 pub struct ApiContext {
     base_url: String,
     http_client: reqwest::Client,
-    pub user_context: Option<UserContext>,
+    user_context: Option<UserContext>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -48,12 +59,65 @@ impl ApiContext {
             user_context: Some(user_context),
         })
     }
+
+    pub async fn tours(
+        &self,
+        start_date: chrono::DateTime<chrono::Utc>,
+        limit: u16,
+    ) -> Result<Vec<Tour>, Box<dyn std::error::Error>> {
+        let context = self.user_context.as_ref().expect(
+            "User context must not be empty. Make sure that auth(...) is called before calling this method.");
+
+        let resp = self
+            .http_client
+            .get(format!(
+                "{0}/v007/users/{1}/tours/",
+                self.base_url, context.user_id
+            ))
+            .basic_auth(&context.email, Some(&context.token))
+            .query(&[
+                ("limit", limit.to_string()),
+                ("page", "0".to_owned()),
+                ("type", "tour_recorded".to_owned()),
+                (
+                    "start_date",
+                    start_date
+                        .with_timezone::<FixedOffset>(
+                            &chrono::FixedOffset::west_opt(7 * 3600).unwrap(),
+                        )
+                        .to_rfc3339_opts(chrono::SecondsFormat::Millis, false),
+                ),
+            ])
+            .send()
+            .await?
+            .error_for_status()?;
+
+        let tours = resp
+            .json::<ToursContainer>()
+            .await?
+            .embedded
+            .map_or(Vec::default(), |e| e.tours);
+
+        Ok(tours)
+    }
 }
 
+#[derive(Debug, Deserialize)]
 pub struct Tour {
     id: u32,
     name: String,
     status: String,
     r#type: String,
     date: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Embedded {
+    tours: Vec<Tour>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ToursContainer {
+    #[serde(rename = "_embedded")]
+    embedded: Option<Embedded>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
+use clap::Parser;
+use komoot::KomootOpts;
+
 mod komoot;
+
+#[derive(Debug, Parser)]
+#[clap(name = "stramoot", version)]
+pub struct Cli {
+    #[clap(flatten)]
+    pub komoot: KomootOpts,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
     let http_client = reqwest::Client::new();
-    let komoot_api = komoot::ApiContext::new("https://api.komoot.de", &http_client);
+    let tours = komoot::ApiContext::new("https://api.komoot.de", &http_client)
+        .auth(&cli.komoot.user_name, &cli.komoot.password)
+        .await?
+        .tours(
+            chrono::Utc::now()
+                .checked_sub_months(chrono::Months::new(6))
+                .unwrap(),
+            10,
+        )
+        .await?;
 
-    let kc = komoot_api.auth("", "").await?;
-
-    match kc.user_context {
-        Some(ct) => println!("{:?}", ct),
-        None => println!("Something is wrong"),
-    }
+    println!("{:?}", tours);
 
     Ok(())
 }
